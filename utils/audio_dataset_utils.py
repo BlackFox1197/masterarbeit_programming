8ad0b4cad5b07ac677ed17630caa1456bda0f5b0
Daniel Lenhardt <ckwdani@gmail.com>
2022-12-13 10:35:11 +0100

    add to audio utils

--- utils/audio_dataset_utils.py
+++ utils/audio_dataset_utils.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 import librosa
 import torchaudio
 
@@ -19,3 +22,49 @@ def speech_file_to_array_librosa(speech_path, target_sampling_rate):
     resampler = librosa.resample(speech_array, orig_sr=sampling_rate, target_sr=target_sampling_rate).squeeze()
 
     return resampler
+
+
+def load_custom_dataset():
+    paths = []
+    testpaths = []
+    testlabels = []
+    terminator = 'D:/Uni/19.Master/Daten/terminator.wav'
+    print(sys.executable)
+    emotions = []
+    # for dirname, _, filenames in os.walk('Daten/TESS Toronto emotional speech set data'):
+    # D:\Uni\19.Master\DATEN
+    for dirname, _, filenames in os.walk('../tess'):
+        for filename in filenames:
+            label = filename.split('_')[-1]
+            label = label.split('.')[0]
+            if (label != 'neutral'):
+                emotions.append(label.lower())
+                paths.append(os.path.join(dirname, filename))
+    for dirname, _, filenames in os.walk('../Stimuli_Intensitätsmorphs'):
+        for filename in filenames:
+
+            intens = filename.split('_')[-2]
+            emot = filename.split('_')[1]
+            label = emot
+            match label:
+                case 'ang':
+                    label = 'angry'
+                case 'dis':
+                    label = 'disgust'
+                case 'fea':
+                    label = 'fear'
+                case 'hap':
+                    label = 'happy'
+                case 'sad':
+                    label = 'sad'
+                case 'sur':
+                    label = 'ps'
+            if (emot != 'ple'):
+                testpaths.append(os.path.join(dirname, filename))
+                testlabels.append(label.lower())
+    com_labels = testlabels + emotions
+    com_paths = testpaths + paths
+    print(testlabels)
+    print(testpaths)
+    print('Dataset is loaded')
+    return paths, emotions, testpaths, testlabels
